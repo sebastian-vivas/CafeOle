a326f46dd3c700718e85d610712bb0be133fcaff
sebastian-vivas <seb.vivas.dev@gmail.com>
2020-03-27 10:42:47 -0400

    Barista App

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://sebVivas:barista@barista-qmymc.mongodb.net/barista?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'barista'
+};
